26ac143d979e3fc6cd40f1ed85cc5b4d79ceb552
Call Carl <callienicole0419@gmail.com>
2022-11-30 17:08:57 -0600

    attached timer to start button

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,14 @@
+// variable for timer
 var timeEl = document.getElementById('timer');
-
 var secondsLeft = 120;
+// variables for event listeners
+var startBtn = document.getElementById('start-button');
 
-function setTime(){
+
+//quiz countdown
+
+function startQuiz(){
+    startBtn.addEventListener('click', function() {
     var timeInterval = window.setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft + ' seconds until quiz is over.';
@@ -10,8 +16,9 @@ function setTime(){
         if (secondsLeft === 0) {
             clearInterval(timeInterval);
             //insert function here
+            timeEl.textContent = 'TIME\'S UP!!';
         }
     }, 1000);
-}
+})};
 
-setTime();
+startQuiz();
