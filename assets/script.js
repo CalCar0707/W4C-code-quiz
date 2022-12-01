fe560adfccfb306db2a7946b2f1c6a50710536d8
Call Carl <callienicole0419@gmail.com>
2022-11-30 22:17:18 -0600

    updated visibility of button and container

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,22 @@ var timeEl = document.getElementById('timer');
 var secondsLeft = 120;
 // variables for event listeners
 var startBtn = document.getElementById('start-button');
+//variables for questions
+var questionContainer = document.querySelector('.container');
+var questionOne = document.getElementById('question1');
+var questionTwo = document.getElementById('question2');
+var questionThree = document.getElementById('question3');
+var questionFour = document.getElementById('question4');
+var questionFive = document.getElementById('question5');
 
+questionContainer.style.display = 'none';
 
 //quiz countdown
 
 function startQuiz(){
-    startBtn.addEventListener('click', function() {
+    startBtn.addEventListener('click', () => {
+    startBtn.style.display = 'none';
+    questionContainer.style.display = 'block';
     var timeInterval = window.setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft + ' seconds until quiz is over.';
@@ -19,6 +29,8 @@ function startQuiz(){
             timeEl.textContent = 'TIME\'S UP!!';
         }
     }, 1000);
-})};
+})
+
+};
 
 startQuiz();
