1c1b26dc70afec9809ec59379384cdf5ed732165
Call Carl <callienicole0419@gmail.com>
2023-01-05 16:31:07 -0600

    added question variable and fixed question display

--- assets/script.js
+++ assets/script.js
@@ -6,56 +6,58 @@ var secondsLeft = 120;
 var startBtn = document.getElementById('start-button');
 
 //variables for questions
-var questionContainer = document.querySelector('.container');
+// questionContainer and question are same element targeted diff ways*****
+// container if for questions and answers
+var container = document.querySelector('.container');
+var questionContainer = document.querySelector('.question-container');
 var answerContainer = document.querySelector('.answer-container');
 var question = document.getElementById('question');
 var answerOne = document.getElementById('answer1');
 var answerTwo = document.getElementById('answer2');
 var answerThree = document.getElementById('answer3');
 var answerFour = document.getElementById('answer4');
+var answerStatus = document.createElement('h4');
 
-questionContainer.style.display = 'none';
+
+container.style.display = 'none';
 
 //variables for question info
 const myQuestions = [
     {   
-        id: 0,
         question: 'Commonly used data types do NOT include:',
         options: ['1: strings', '2. booleans', '3. alerts', '4. numbers'],
-        answer: '3'
+        answer: '3. alerts'
     },
     {
-        id: 1,
         question: 'Arrays in JavaScript can be used to store ______.',
         options: ['1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above'],
-        answer: '4'
+        answer: '4. all of the above'
     },
     {
-        id: 2,
         question: 'The condition of an if/ else statement is enclosed with _____.',
         options: ['1. quotes', '2. curly brackets', '3. parentheses', '4. square brackets'],
-        answer: '2'
+        answer: '2. curly brackets'
     },
     {
-        id: 3,
         question: 'The first index of an array is: ',
         options: ['1. [0]', '2. [1]', '3. [2]', '4. [8]'],
-        answer: '1'
+        answer: '1. [0]'
     },
     {
         question: 'How do you create a function?',
         options: ['1. function = myFunction()', '2. function myFunction()', '3. myFunction()', '4. function: myFunction()'],
-        answer: '2'
-    }
-]
+        answer: '2. function myFunction()'
+    }];
+
+var score = 0;
 
 
-//quiz countdown
+//quiz countdown- WORKING
 function countdown(){
     //clicking start button starts
     startBtn.addEventListener('click', () => {
     startBtn.style.display = 'none';
-    questionContainer.style.display = 'block';
+    container.style.display = 'block';
     var timeInterval = window.setInterval(function () {
         secondsLeft--;
         timeEl.textContent = 'Time: ' + secondsLeft;
@@ -63,11 +65,33 @@ function countdown(){
         if (secondsLeft === 0) {
             clearInterval(timeInterval);
             //insert function here
+
             timeEl.textContent = 'TIME\'S UP!!';
         }
     }, 1000);
 })
 
+   //for(var i=0; i < myQuestions.length; i++){
+    question.textContent = myQuestions[0].question;
+    answerOne.textContent = myQuestions[0].options[0];
+    answerTwo.textContent = myQuestions[0].options[1];
+    answerThree.textContent = myQuestions[0].options[2];
+    answerFour.textContent = myQuestions[0].options[3];
+
+    container.append(question);
+    container.append(answerOne);
+    container.append(answerTwo);
+    container.append(answerThree);
+    container.append(answerFour);
+
+    
+
+  
+    //if(response === myQuestions.question1.answer) {
+      //  score++;
+    //} else {
+      //  secondsLeft - 10;
+    //}
 };
 
 
